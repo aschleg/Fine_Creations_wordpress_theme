2dca98b61e9363945ef6afd70114830aa3eb0fa0
Aaron Schlegel <aaronsc32@gmail.com>
2014-04-24 16:29:34 -0700

    working on filterable portfolio list

--- portfolio.php
+++ portfolio.php
@@ -7,30 +7,40 @@ Template Name: Portfolio
 <?php get_header(); ?>
 
 <div class="row">
+	<div class="portfolio-projects">
+		<ul>
 
-		<?php
-			$wp_query_portfolio = new WP_Query();
-			$wp_query_portfolio->query('category_name='.portfolio.'&paged='.$paged);
-		?>
+			<?php
+				$wp_query_portfolio = new WP_Query();
+				$wp_query_portfolio->query('category_name='.portfolio.'&paged='.$paged);
+			?>
 
-		<?php
-			if( $wp_query_portfolio->have_posts() ) :
-			while( $wp_query_portfolio->have_posts() ) :
-			$wp_query_portfolio->the_post();
-		?>
+			<?php
+				if( $wp_query_portfolio->have_posts() ) :
+				while( $wp_query_portfolio->have_posts() ) :
+				$wp_query_portfolio->the_post();
+			?>
 
-	<div class="col-xs-4 col-md-3">
+			<li class="col-xs-4 col-md-4"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('portfolio'); ?></a>
+				<div class="caption">
+					<p><?php the_title(); ?></p>
+					<p><?php exclude_post_categories('49'); ?></p>
+				</div>
+			</li>
+			<?php endwhile; ?>
+			<?php wp_reset_postdata(); ?>
+			<?php else: endif; ?>
+
+		</ul>
+
+			<!--<div class="col-xs-4 col-md-3">
 			<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('portfolio'); ?></a>
 			<div class="caption">
 				<p><?php the_title(); ?></p>
 				<p><?php exclude_post_categories('49'); ?></p>
 			</div>
-	</div>
-
-		<?php endwhile; ?>
-		<?php wp_reset_postdata(); ?>
-		<?php else: endif; ?>
-
+	</div>-->
+</div>
 </div>
 </section>
 <?php get_footer(); ?>
